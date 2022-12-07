c3c9d91e00642c627d43ca753f5e177cf4218ad9
Fábio Vital <fabiovital@tecnico.ulisboa.pt>
2022-12-07 11:01:06 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp -rf $HOME/.dotfiles/.p10k.zsh $HOME
+cp -rf $HOME/.dotfiles/.profile $HOME
+cp -rf $HOME/.dotfiles/.vimrc $HOME
+cp -rf $HOME/.dotfiles/.zshrc $HOME
+cp -rf $HOME/.dotfiles/settings.json $HOME/gitpod/.vscode-server/data/Machine
